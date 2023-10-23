c08aa2e44f870bac872c3f74900759961b33ef23
Mulinatti <gabrielmulinari2002@gmail.com>
2023-10-23 20:19:02 -0300

    api connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import PokeCard from "./components/PokeCard/PokeCard";
 
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const getPokemon = async (id) => {
+      try {
+        const url = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10`);
+        const data = await url.json();
+
+        setPokemons(data.results);
+        console.log(pokemons);
+      } catch (erro) {
+        console.error(erro);
+      }
+    }
+    getPokemon();
+  }, [])
+
   return (
-    <main className="p-5">
-      <PokeCard/>
+    <main className="p-5 grid grid-cols-6">
+      {pokemons.map(poke => <PokeCard key={poke.name} name={poke.name}/>)}
     </main>
   );
 }
--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 
 const PokeCard = (props) => {
   return (
-    <div className="p-3 max-w-[200px] shadow-card-shadow rounded-md hover:scale-[1.01] transition-all">
+    <div className="p-3 mb-5 max-w-[200px] shadow-card-shadow rounded-md hover:scale-[1.01] transition-all">
         <div className="flex justify-center">
             <img className="w-[150px] h-[150px]" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/553.png" alt="imagem_pokemon"/>
         </div>
         <hr className="border-black/20 my-2" />
         <div className="flex flex-col justify-between items-center h-[70px]">
-            <p>Krookodile</p>
+            <p className="capitalize">{props.name}</p>
             <div className="flex justify-center min-w-[170px]">
                 <div className="flex justify-between w-full">
-                    <div className="bg-stone-700 card_pokemon">Dark</div>
-                    <div className="bg-orange-300 card_pokemon">Ground</div>
+                    <div className={`card_pokemon`}></div>
                 </div>
             </div>
         </div>
